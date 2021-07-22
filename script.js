fbd07f0c5599bfbf7db3aef98a8443b8aa17e3af
Rafael Trindade <rafaeltrindade@outlook.com>
2021-07-22 18:25:07 -0300

    feat: adicionado stop e play manual; bugfix: corrigido problema de não pausa musica anterior quando é selecionado uma música antes da musica atual

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
-// const button1 = document.getElementById("button1");
-
-// let audio1 = new Audio();
-// audio1.src = "assets/musics/A Vitoria Chegou.mp3";
-// let audio2 = new Audio();
-// audio2.src = "assets/musics/Eu Vencerei.mp3";
-// console.log(audio1)
-
-// button1.addEventListener('click', function() {
-//   audio1.play();
-//   audio1.addEventListener('playing', function() {
-//     console.log('Audio 1 started playing!');
-//   });
-//   audio1.addEventListener('ended', function() {
-//     console.log('Audio 1 ended!');
-//   })
-// })
-
-let player = document.querySelector('.player');
-let musica = document.getElementsByTagName('p');
-const lista = ["assets/musics/A Vitoria Chegou.mp3","assets/musics/Cem Ovelhas.mp3", "assets/musics/Diário de um Vencedor.mp3", "assets/musics/Não Pare.mp3", "assets/musics/o maior troféu.mp3", "assets/musics/Eu Vencerei.mp3"] 
-console.log(player);
-let track = 0;
-
-player.src = lista[track];
-
+//DA PAUSE NA ANTIGA E EXECUTA MUSICA SELECIONADA
 const selectMusic = function () {
-  console.log(lista[this.dataset.n])
-  player.src = lista[this.dataset.n];
-  player.play();
+  faixa[track].pause();
+  faixa[track].currentTime = 0;
+
+  faixa[this.dataset.n].play();
+
   track = this.dataset.n;
+
+  player.innerHTML = `<p>Musica ${parseInt(track) + 1}</p>` //NÃO TA PRONTO
 }
 
-player.addEventListener('ended', function() {
-  track++;
-  player.src = lista[track];
-  player.play();
-})
+//PEGANDO HTMLS
+let player = document.querySelector('.player'); //NÃO TA PRONTO
+let musicaName = document.getElementsByTagName('p');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
 
-musica[0].addEventListener('click', selectMusic);
-musica[1].addEventListener('click', selectMusic);
-musica[2].addEventListener('click', selectMusic);
-musica[3].addEventListener('click', selectMusic);
-musica[4].addEventListener('click', selectMusic);
-musica[5].addEventListener('click', selectMusic);
+//VARIAVEIS
+const lista = ["assets/musics/A Vitoria Chegou.mp3","assets/musics/Cem Ovelhas.mp3", "assets/musics/Diário de um Vencedor.mp3", "assets/musics/Não Pare.mp3", "assets/musics/o maior troféu.mp3", "assets/musics/Eu Vencerei.mp3"] 
+let track = 0;
+let faixa = [];
 
+
+
+
+lista.forEach((element, item) => {
+  //CRIANDO ARRAY COM MUSICAS
+  faixa[item] = new Audio(element);
+
+  //QUANDO CLICA NA MUSICA ELE CHAMA A FUNÇÃO QUE EXECUTA A MUSICA CLICADA
+  musicaName[item].addEventListener('click', selectMusic);
+
+  //AVANÇO AUTOMTICO DA MÚSICA
+  faixa[item].addEventListener('ended', function() {
+    track++;
+    faixa[track].play();
+  })
+
+  //ADD FUNÇÃO CLICK
+  play.addEventListener('click', function () {
+    faixa[item].play();
+  })
+
+  stop.addEventListener('click', function () {
+    faixa[item].pause();
+  })
+
+});
 
