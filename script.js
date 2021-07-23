2c9b5f614d35efb6bd057f29db141e55bc1cd2ac
Rafael Trindade <rafaeltrindade@outlook.com>
2021-07-23 16:07:31 -0300

    ajuste visual

--- script.js
+++ script.js
@@ -1,46 +1,71 @@
 //DA PAUSE NA ANTIGA E EXECUTA MUSICA SELECIONADA
 const selectMusic = function () {
+  musicas[track].controls = false;
+  musicas[track].pause();
+  musicas[track].currentTime = 0;
+
+  console.log(musicas[track]);
+
   track = this.dataset.n;
-  console.log(faixa[track]);
+  // console.log(faixa[track]);
 
   // faixa[track].controls = true;
-  faixa[track].autoplay = true;
-  player.innerHTML = faixa[track].outerHTML;
+  musicas[track].controls = true;
+  musicas[track].play();
 
-  musicas[0].addEventListener('ended', proximaMusica);
   musicNumber[0].innerHTML = `Música ${parseInt(track)+1}`;
-
 }
 
 //PEGANDO HTMLS
 let player = document.querySelector('.player'); 
-let musicas = document.getElementsByTagName('audio');
+let musicas = player.getElementsByTagName('audio');
+let musicInit = musicas[0].outerHTML;
 let musicList = document.getElementsByTagName('li');
 let musicNumber = document.getElementsByClassName('music-number');
+
+
+player.innerHTML = ""; //apaga o player após pegar a variavél audio
 
 //VARIAVEIS
 const lista = ["assets/musics/A Vitoria Chegou.mp3","assets/musics/Cem Ovelhas.mp3", "assets/musics/Diário de um Vencedor.mp3", "assets/musics/Não Pare.mp3", "assets/musics/o maior troféu.mp3", "assets/musics/Eu Vencerei.mp3"] 
 let track = 0;
 let faixa = [];
 
+//CRIANDO TAG AUDIO DAS MUSICAS
 lista.forEach((element, item) => {
   //CRIANDO ARRAY COM MUSICAS
-  faixa[item] = new Audio(element);
-  faixa[item].controls = true;
+  player.innerHTML += musicInit; //a div player recebe a tag audio configurada em forma de string
+  player.getElementsByTagName('audio')[item].src = element; //adiciona o endereço da música
+  
+
   //QUANDO CLICA NA MUSICA ELE CHAMA A FUNÇÃO QUE EXECUTA A MUSICA CLICADA
   musicList[item].addEventListener('click', selectMusic);
   console.log(musicList[item])
+
+  musicas[item].addEventListener('ended', proximaMusica);
+
 });
 
-player.innerHTML = faixa[0].outerHTML;
+console.log(musicas);
+console.log(player);
+
+musicas[0].controls = true;
+
+// player.innerHTML = faixa[0].outerHTML;
 musicas[0].addEventListener('ended', proximaMusica);
 musicNumber[0].innerHTML = `Música ${parseInt(track)+1}`;
 
 
 function proximaMusica () {
+
+  musicas[track].controls = false;
+  musicas[track].pause();
+  musicas[track].currentTime = 0;
+
   track++;
-  faixa[track].autoplay = true;
-  player.innerHTML = faixa[track].outerHTML;
-  musicas[0].addEventListener('ended', proximaMusica);
+
+  musicas[track].controls = true;
+  musicas[track].play();
+
   musicNumber[0].innerHTML = `Música ${parseInt(track)+1}`;
 }
